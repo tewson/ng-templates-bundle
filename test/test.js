38343780972d8db3e1e8113f834b634dfe7c37f2
Tewson Seeoun <tewson.seeoun@gmail.com>
2016-10-20 02:05:33 +0100

    Add a test for template inclusion

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
-/* global describe it */
+/* global describe it before */
 
 var jsdom = require('jsdom').jsdom
 
-global.document = jsdom('<html><head><script></script></head><body></body></html>')
+global.document = jsdom('<html><head><script></script></head><body><template-tester></template-tester></body></html>')
 global.window = document.defaultView
 
 require('angular/angular')
@@ -18,5 +18,21 @@ describe('Template module', function () {
   it('should have been registered', function () {
     expect(angular.module('templates')).to.not.be.undefined
     expect(angular.module('templates').name).to.equal('templates')
+  })
+})
+
+describe('Templated component', function () {
+  before(function () {
+    angular.module('test', [ 'templates' ])
+      .component('templateTester', {
+        templateUrl: 'test/template.html'
+      })
+    angular.bootstrap(document.body, [ 'test' ])
+    this.$rootScope = angular.element(document.body).scope().$root
+    this.$rootScope.$apply()
+  })
+
+  it('should contain template content', function () {
+    expect(document.querySelector('body').innerHTML).to.have.string('The quick brown fox jumps over the lazy dog.')
   })
 })
