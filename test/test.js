ddd428f96e0c7507a581d26e564466d3992733fa
Tewson Seeoun <tewson.seeoun@gmail.com>
2016-11-03 23:33:42 +0000

    Add unit tests for bundling multiple templates

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 /* global describe it before */
 
+var fs = require('fs')
+var shell = require('shelljs')
 var jsdom = require('jsdom').jsdom
 
 global.document = jsdom('<html><head><script></script></head><body><template-tester></template-tester></body></html>')
@@ -34,5 +36,35 @@ describe('Templated component', function () {
 
   it('should contain template content', function () {
     expect(document.querySelector('body').innerHTML).to.have.string('The quick brown fox jumps over the lazy dog.')
+  })
+})
+
+describe('Generated template file', function () {
+  before(function () {
+    this.randomTemplateContent = {}
+    this.templatePaths = [
+      'tmp/multiple-templates-1.html',
+      'tmp/multiple-templates-2.html'
+    ]
+
+    this.templatePaths.forEach(function (templateFilePath) {
+      this.randomTemplateContent[templateFilePath] = Math.random().toString()
+      fs.writeFileSync(templateFilePath, this.randomTemplateContent[templateFilePath])
+    }.bind(this))
+
+    shell.exec('./bin/main.js --paths "tmp/multiple-templates*.html" --standalone > tmp/template.js')
+
+    this.generatedTemplate = fs.readFileSync('tmp/template.js').toString()
+  })
+
+  it('should create only one module', function () {
+    expect((this.generatedTemplate.match(/angular\.module/g) || []).length).to.equal(1)
+  })
+
+  it('should contain the template content', function () {
+    this.templatePaths.forEach(function (templatePath) {
+      var templateContentRegExp = new RegExp(this.randomTemplateContent[templatePath], 'g')
+      expect(templateContentRegExp.test(this.generatedTemplate)).to.be.true
+    }.bind(this))
   })
 })
